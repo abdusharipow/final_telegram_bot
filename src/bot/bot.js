c8f4185a9df8c90173979d838be4fa9b1db06cfc
shoxrux <shoxruxabdusharipovv@gmail.com>
2025-12-12 17:21:09 +0500

    done new codes

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -1,11 +1,10 @@
 
 import TelegramBot from "node-telegram-bot-api";
 import dotenv from "dotenv";
-import onCommands from "./handlers/message/onCommands.js";
-import onError from "./handlers/message/onError.js";
+import onCommands from "./hendlers/message/onCommands.js";
+import onError from "./hendlers/message/onError.js";
 dotenv.config();
-const CHANNEL_ID = "@academy_100x_uz";
-
+const CHANNEL_ID = "@neww_kanall";
 export const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
 bot.on("message", async function (msg) {
@@ -20,15 +19,15 @@ bot.on("message", async function (msg) {
   if (chatMember.status == "kicked" || chatMember.status == "left") {
     return bot.sendMessage(
       chatId,
-      `Oldin shu kanalga obuna bo'ling @academy_100x_uz`,
+      `Oldin shu kanalga obuna bo'ling @neww_kanall`,
       {
         reply_markup: {
           remove_keyboard: true,
           inline_keyboard: [
             [
               {
-                text: "100x Academy Xiva",
-                url: "https://t.me/academy_100x_uz",
+                text: "@neww_kanall",
+                url: "https://t.me/neww_kanall",
               },
             ],
             [
@@ -45,6 +44,57 @@ bot.on("message", async function (msg) {
 
   if (text.startsWith("/")) {
     return onCommands(msg);
+  } else if (text == "📚 Kurslar") {
+     bot.sendMessage(chatId, `🎓 Bizning o‘quv markazimizda quyidagi kurslar mavjud:`, {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: "🇬🇧 Ingliz tili", callback_data: "english" }],
+          [{ text: "🇷🇺 Rus tili", callback_data: "russian" }],
+          [{ text: "🧮 Matematika", callback_data: "math" }],
+          [{ text: "💻 Dasturlash", callback_data: "it" }],
+          [{ text: "🎨 Grafik dizayn", callback_data: "design" }],
+        ]
+      }
+    });
+  }   else if (text == "ℹ️ Markaz haqida") {
+    bot.sendMessage(chatId, `
+      ℹ️ MARKAZ HAQIDA
+
+🎓 100x o‘quv markazi
+📍 Manzil: Xiva IT PARK ichida
+⏰ Ish vaqti: Dush–Yak, 9:00–19:00
+📞 .........
+      `)
+  }  else if (text == "💬 Fikr bildirish") {
+    bot.sendMessage(chatId,
+      `
+      💬 Marxamat, o‘quv markazimiz haqida fikr qoldirishingiz mumkin.
+
+Quyidagilardan birini tanlang 👇
+      `, {
+      reply_markup: {
+        inline_keyboard: [
+          [{ text: "⭐ Baxolash", callback_data: "baxolash" }],
+          [{ text: "✍️ Matn yozish", callback_data: "matn yozish" }]
+        ]
+      }
+    })
+  }  else if (text == "❓ Yordam") {
+    bot.sendMessage(chatId, `
+      Yordam uchun Admin ga murojat qiling
+      Admin:  @shoxruxx_abdushar1povv
+      `)
+  }  else {
+    bot.sendMessage(
+      chatId,
+      `
+     ⚠️ Kechirasiz, men sizning xabaringizni tushunmadim.
+
+ Iltimos, quyidagi tugmani bosing 👇
+  /start
+
+     `
+    );
   }
 
   return onError();
@@ -54,16 +104,107 @@ bot.on("callback_query", async function (query) {
   const chatId = query.message.chat.id;
   const firstname = query.message.chat.first_name;
   const data = query.data;
-
+  if (data === "english") {
+       bot.sendMessage(chatId, `
+      🇬🇧 Ingliz tili kursi (IELTS tayyorlov)
+📘 Maqsad: 5.5 dan 7.0 gacha olib chiqish
+⏳ Davomiyligi: IELTS olguncha
+💵 Narxi: 500 ming so‘m / oyiga
+👨‍🏫 Ustoz: ....
+      `, {
+      reply_markup: {
+        inline_keyboard: [[{ text: "✍️ Kursga yozilish", callback_data: "yozilish" }]]
+      }
+    });
+    } else if (data === 'russian') {
+    bot.sendMessage(chatId, `
+      🇷🇺 Rus tili (Suhbat darajasi)
+      📘 Maqsad: Ish / o‘qish uchun so‘zlashuv darajasi
+⏳ 2 oy, haftasiga 3 marta
+💵 400 ming so‘m / oy
+👨‍🏫 Ustoz: .....
+      `, {
+      reply_markup: {
+        inline_keyboard: [[{ text: "✍️ Kursga yozilish", callback_data: "yozilish" }]]
+      }
+    });
+  } else if (data == "math") {
+    bot.sendMessage(chatId, `
+      🧮 Matematika (maktab va abituriyentlar uchun)
+🎯 Maqsad: Formulalarni to‘liq tushunish va test yechish
+⏳ 4 oy
+💵 450 ming so‘m / oy
+👨‍🏫 Ustoz: .....
+      `, {
+      reply_markup: {
+        inline_keyboard: [[{ text: "✍️ Kursga yozilish", callback_data: "yozilish" }]]
+      }
+    });
+  } else if (data == "it") {
+    bot.sendMessage(chatId, `
+          💻 Dasturlash (Frontend va Backend)
+🎯 Maqsad: 0 dan Junior darajaga
+⏳ 6 oy
+💵 600 ming so‘m / oy
+👨‍🏫 Mentor: ....
+      `, {
+      reply_markup: {
+        inline_keyboard: [[{ text: "✍️ Kursga yozilish", callback_data: "yozilish" }]]
+      }
+    });
+  } else if (data == "design") {
+    bot.sendMessage(chatId, `
+          🎨 Grafika dizayn (Adobe, Canva)
+🎯 Maqsad: Logo, banner, post tayyorlashni o‘rganish
+⏳ 3 oy
+💵 500 ming so‘m / oy
+👨‍🏫 Ustoz: ....
+      `, {
+      reply_markup: {
+        inline_keyboard: [[{ text: "✍️ Kursga yozilish", callback_data: "yozilish" }]]
+      }
+    });
+  }  else if (data == "baxolash") {
+    bot.sendMessage(
+      chatId,
+      `
+Nechchi ball bilan baholaysiz?
+Iltimos, quyidagi yulduzlardan birini tanlang ⭐
+      `,
+      {
+        reply_markup: {
+          inline_keyboard: [
+            [{ text: "⭐ 1", callback_data: "1" }],
+            [{ text: "⭐⭐ 2", callback_data: "2" }],
+            [{ text: "⭐⭐⭐ 3", callback_data: "3" }],
+            [{ text: "⭐⭐⭐⭐ 4", callback_data: "4" }],
+            [{ text: "⭐⭐⭐⭐⭐ 5", callback_data: "5" }]
+          ]
+        },
+      }
+    );
+  } else if (data == "1") {
+    bot.sendMessage(chatId, `Raxmat,balingiz qabul qilindi ✅`)
+  } else if (data == "2") {
+    bot.sendMessage(chatId, `Raxmat,balingiz qabul qilindi ✅`)
+  } else if (data == "3") {
+    bot.sendMessage(chatId, `Raxmat,balingiz qabul qilindi ✅`)
+  } else if (data == "4") {
+    bot.sendMessage(chatId, `Raxmat,balingiz qabul qilindi ✅`)
+  } else if (data == "5") {
+    bot.sendMessage(chatId, `Raxmat,balingiz qabul qilindi ✅`)
+  } else if (data == "matn yozish") {
+    bot.sendMessage(chatId, `Fikr yozing `)
+  }
   if (data == "confirm_subscription") {
     const chatMember = await bot.getChatMember(CHANNEL_ID, chatId);
-
+    
     console.log(chatMember);
 
     if (chatMember.status == "kicked" || chatMember.status == "left") {
       return bot.sendMessage(
         chatId,
-        `Oldin shu kanalga obuna bo'ling @academy_100x_uz`,
+        `Oldin shu kanalga obuna bo'ling @neww_kanall`,
         {
           reply_markup: {
             remove_keyboard: true,
@@ -71,7 +212,7 @@ bot.on("callback_query", async function (query) {
               [
                 {
                   text: "100x Academy Xiva",
-                  url: "https://t.me/academy_100x_uz",
+                  url: "https://t.me/neww_kanall",
                 },
               ],
               [
@@ -84,7 +225,7 @@ bot.on("callback_query", async function (query) {
           },
         }
       );
-    }
+    } 
   }
 });
 
